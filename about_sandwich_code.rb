0e4eecffca3675514166fb8bb52b8ea7435f0743
Calin <calinoiu.alexandru@gmail.com>
2012-02-18 10:57:43 +0200

    about_sandwich has expanded my awareness

--- about_sandwich_code.rb
+++ about_sandwich_code.rb
@@ -5,7 +5,7 @@ class AboutSandwichCode < EdgeCase::Koan
   def count_lines(file_name)
     file = open(file_name)
     count = 0
-    while line = file.gets
+    while file.gets
       count += 1
     end
     count
@@ -14,14 +14,14 @@ class AboutSandwichCode < EdgeCase::Koan
   end
 
   def test_counting_lines
-    assert_equal __, count_lines("example_file.txt")
+    assert_equal 4, count_lines("example_file.txt")
   end
 
   # ------------------------------------------------------------------
 
   def find_line(file_name)
     file = open(file_name)
-    while line = file.gets
+    while (line = file.gets)
       return line if line.match(/e/)
     end
   ensure
@@ -29,7 +29,7 @@ class AboutSandwichCode < EdgeCase::Koan
   end
 
   def test_finding_lines
-    assert_equal __, find_line("example_file.txt")
+    assert_equal "test\n", find_line("example_file.txt")
   end
 
   # ------------------------------------------------------------------
@@ -66,7 +66,7 @@ class AboutSandwichCode < EdgeCase::Koan
   def count_lines2(file_name)
     file_sandwich(file_name) do |file|
       count = 0
-      while line = file.gets
+      while file.gets
         count += 1
       end
       count
@@ -74,17 +74,21 @@ class AboutSandwichCode < EdgeCase::Koan
   end
 
   def test_counting_lines2
-    assert_equal __, count_lines2("example_file.txt")
+    assert_equal 4, count_lines2("example_file.txt")
   end
 
   # ------------------------------------------------------------------
 
   def find_line2(file_name)
-    # Rewrite find_line using the file_sandwich library function.
+    file_sandwich(file_name) do |file|
+      while (line = file.gets)
+        return line if line.match(/e/)
+      end
+    end
   end
 
   def test_finding_lines2
-    assert_equal __, find_line2("example_file.txt")
+    assert_equal "test\n", find_line2("example_file.txt")
   end
 
   # ------------------------------------------------------------------
@@ -92,7 +96,7 @@ class AboutSandwichCode < EdgeCase::Koan
   def count_lines3(file_name)
     open(file_name) do |file|
       count = 0
-      while line = file.gets
+      while file.gets
         count += 1
       end
       count
@@ -100,7 +104,7 @@ class AboutSandwichCode < EdgeCase::Koan
   end
 
   def test_open_handles_the_file_sandwich_when_given_a_block
-    assert_equal __, count_lines3("example_file.txt")
+    assert_equal 4, count_lines3("example_file.txt")
   end
 
 end
